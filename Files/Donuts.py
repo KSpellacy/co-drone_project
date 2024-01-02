537f739de5b7e34347ccea2be80a0b6503b90e5f
cspellacy <spellacy@gmail.com>
2024-01-02 13:42:18 -0500

    adding Donuts.py from 1st winter practice

---
+++ Files/Donuts.py
@@ -0,0 +1,22 @@
+from codrone_edu.drone import *
+
+
+def pair():
+    drone = Drone()
+    drone.pair()
+    drone.reset_trim()
+    drone.reset_sensor()
+    drone.reset_move()
+    return drone
+
+
+drone = pair()
+drone.takeoff()
+
+drone.send_absolute_position(3.04, 0, 1.6, 1, 0, 0)
+time.sleep(5)
+drone.send_absolute_position(3.04, 1.8, 1.0, 1, 0, 0)
+time.sleep(5)
+
+drone.land()
+drone.disconnect()
